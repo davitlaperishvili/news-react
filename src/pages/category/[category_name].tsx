fc31b876bdcbfd53ddb6ab519104be6a1873e2d0
dlaperishvili <davitlaperishvili@gmail.com>
2023-01-24 16:47:10 +0400

    categories in progress

---
+++ src/pages/category/[category_name].tsx
@@ -0,0 +1,16 @@
+import PageTitle from '@/components/pageParts/PageTitle'
+import MainContainer from '@/layouts/MainContainer/MainContainer'
+import PageWrapper from '@/layouts/PageWrapper'
+import React from 'react'
+
+export default function category_name() {
+  return (
+    <>
+    <PageWrapper>
+      <MainContainer>
+        <PageTitle title="Category Page"/>
+      </MainContainer>
+    </PageWrapper>
+    </>
+  )
+}
